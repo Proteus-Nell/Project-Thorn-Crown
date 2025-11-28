b39f49d02bd839c4bf0346a72f3b1ec6b0ff44de
Mahmoud Yasser Mokhtar Sallam <efyms24@nottingham.edu.my>
2025-11-29 02:18:35 +0800

    Create Launcher.java

---
+++ src/main/java/com/comp2042/Launcher.java
@@ -0,0 +1,8 @@
+package com.comp2042;
+
+// Launcher class for testing purposes to circumnavigate jfx runtime errors.
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
